fe7d5c519577ef900436518ce1b854dc0f40ef15
k0psutin <mr.gravez@gmail.com>
2021-06-04 09:41:34 +0300

    Add authorization header routing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { ApolloServer } = require('apollo-server')
-const { ApolloGateway } = require('@apollo/gateway')
+const { ApolloGateway, RemoteGraphQLDataSource } = require('@apollo/gateway')
 
 require('dotenv').config()
 
@@ -10,19 +10,35 @@ const gateway = new ApolloGateway({
   serviceList: [
     { name: 'user-api', url: process.env.USER_API },
   ],
+  buildService({ name, url }) {
+    return new RemoteGraphQLDataSource({
+      url,
+      willSendRequest({ request, context }) {
+        if (context.authorization) {
+          request.http.headers.set(
+            'authorization',
+            context.authorization,
+          )
+        }
+      },
+    })
+  },
 })
 
 const server = new ApolloServer({
   gateway,
   engine: false,
   subscriptions: false,
+  context: ({ req }) => {
+    return { authorization: req.headers.authorization }
+  },
   plugins: [
     {
       requestDidStart: (requestContext) => {
         if ( requestContext.request.http?.headers.has( 'x-apollo-tracing' ) ) {
           return
         }
-        if (ENV !== 'production') {
+        if (ENV !== 'development') {
           const query =
         requestContext.request.query?.replace( /\s+/g, ' ' ).trim()
           const variables = JSON.stringify( requestContext.request.variables )
@@ -40,7 +56,7 @@ const server = new ApolloServer({
 })
 
 server.listen({ port: PORT }).then(({ url }) => {
-  console.log(`Server ready at ${url}`)
+  console.log(`gateway <- ready at ${url}`)
 }).catch((err) => {
   console.log('gateway <- cannot connect to service(s)')
   console.log('gateway <-', err.message)
