0750050f57759d1e7767b1c8ad99a35a8c3fc48c
Erkka Rahikainen <ergga96@gmail.com>
2021-06-11 10:13:16 +0300

    Remove unused script

--- scripts/deploymentId.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-aws deploy list-deployments \
-              --application-name user-api-deployer \
-              --deployment-group-name Gateway-api-deployment-group \
-              --query 'deployments' \
-              --max-items 1 \
-              | sed 's/^"\(.*\)".*/\1/'
